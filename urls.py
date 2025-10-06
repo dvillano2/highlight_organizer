3e370f5791065046ce9592dfff2956404024950f
dvillano2 <domvillano@gmail.com>
2025-10-06 13:41:03 -0400

    changed formatting

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 """Urls to pull schedule info, multiples so the corroborate each other"""
 
-from typing import Dict, List
+from typing import Dict
 
-URLS2526 = [
-        ("https://www.premierleague.com/en/news/4324539/"
-         "all-380-fixtures-for-202526-premier-league-season"),
-]
+URLS2526: Dict[str, str] = {
+    "official": (
+        "https://www.premierleague.com/en/news/4324539/"
+        "all-380-fixtures-for-202526-premier-league-season"
+    ),
+}
 
-URLS_BY_SEASON: Dict[str, List[str]] = {"2526": URLS2526}
+URLS_BY_SEASON: Dict[str, Dict[str, str]] = {"2526": URLS2526}
