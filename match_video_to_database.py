ea0cb6db3b5fc2624cae7208c875280ce85ebfa4
dvillano2 <domvillano@gmail.com>
2025-10-13 13:03:33 -0400

    beginning of matching db with youtube

--- match_video_to_database.py
+++ match_video_to_database.py
@@ -1,5 +1,8 @@
 from typing import Dict
 from typing import List
+from typing import Tuple
+import re
+from patterns import team_regex
 
 
 def filter_for_highlights(video_info: list[dict]) -> list[dict]:
@@ -25,3 +28,47 @@ def match_dates(video_info: List[Dict], dates: list) -> List[Dict]:
         for info in video_info
         if any(date in info["title"] for date in formatted_dates)
     ]
+
+
+def format_games(
+    games_info: List[Tuple[str, str, str, str]]
+) -> List[Tuple[re.Pattern, re.Pattern, re.Pattern, str]]:
+    "assumes games info comes from a selece full_date, home, away ,id query"
+    regex = {
+        team_name: re.compile(team_regex)
+        for team_name, team_regex in team_regex().items()
+    }
+    return [
+        (
+            re.compile(format_date_for_displayed_comparison(game[0])),
+            regex[game[1]],
+            regex[game[2]],
+            game[3],
+        )
+        for game in games_info
+    ]
+
+
+def match_games_to_videos(
+    video_info: List[Dict],
+    games_info: List[Tuple[str, str, str, str]],
+) -> Tuple[Dict[str, str], List[str]]:
+    """
+    this is naive with the double loop, various ways to make it better
+    but everything is so small that it doesn't really matter
+    still, better to sort by date desending in the query
+    """
+    formatted_games = format_games(games_info)
+    id_url: Dict[str, str] = {}
+    missed_games = []
+    for game in formatted_games:
+        game_id = game[3]
+        matching_items = game[:3]
+        for video in video_info:
+            if all(
+                pattern.search(video["title"]) for pattern in matching_items
+            ):
+                id_url[game_id] = video["url"]
+                break
+        missed_games.append(game_id)
+    return id_url, missed_games
