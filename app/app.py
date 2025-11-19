68cb6064e37d200b1749dad6815dac8e8c938825
dvillano2 <domvillano@gmail.com>
2025-11-19 13:42:50 -0500

    got rough layout working

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from pull_info import make_dict, pull_db_info
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    rows = pull_db_info()
+    highlights = make_dict(rows)
+    return render_template("highlights.html", highlights=highlights)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
