9896074fdce58f3b7347fd319fec1ecc7c7e866d
dvillano2 <domvillano@gmail.com>
2025-11-30 17:46:03 -0500

    local/production separation

--- app/db.py
+++ app/db.py
@@ -1,13 +1,16 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+if os.environ.get("RENDER") is None:
+    load_dotenv()
+    LOCAL_URL = os.environ.get("LOCAL_DB_URL")
+    local_engine = create_engine(LOCAL_URL, echo=True, future=True)
+    LocalSession = sessionmaker(
+        bind=local_engine, autoflush=False, autocommit=False
+    )
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-LOCAL_URL = os.environ.get("LOCL_DB_URL")
-local_engine = create_engine(LOCAL_URL, echo=True, future=True)
-LocalSession = sessionmaker(
-    bind=local_engine, autoflush=False, autocommit=False
-)
