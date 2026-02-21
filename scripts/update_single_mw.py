b4c26fed2311d5fd0e559496367b1f96248c2bb7
dvillano2 <domvillano@gmail.com>
2026-02-21 14:31:58 -0500

    imports

--- scripts/update_single_mw.py
+++ scripts/update_single_mw.py
@@ -1,5 +1,5 @@
-from pull_season_data import pull_single_mw, mws_to_df
-from update_season_data import update_query
+from scripts.pull_season_data import pull_single_mw, mws_to_df
+from scripts.update_season_data import update_query
 from sqlalchemy import text
 from app.db import engine, Session, local_engine, LocalSession
 from sqlalchemy.exc import SQLAlchemyError
